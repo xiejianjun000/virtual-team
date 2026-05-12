ed34c9bfd9eeb9c7ef2e7f0103de8340f363ac51
xiejianjun000 <xiejianjun000@users.noreply.github.com>
2026-05-12 02:01:42 +0000

    chore: 初始化 Monorepo 项目结构和 Prisma 配置

---
+++ packages/config/eslint/base.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+};
