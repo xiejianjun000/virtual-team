55cce84bd6787e431aa44d956a03d05e27447102
Junge <awep000@qq.com>
2026-05-13 17:27:00 +0800

    fix: ESLint v9 flat config migration
    - Migrated @virtualteam/config/eslint/base.js to flat config
    - Migrated apps/web to eslint.config.js
    - Updated ESLint to ^9.0.0
    
    Co-authored-by: CI Bot <bot@xiejianjun000.com>

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -1,10 +1,14 @@
-module.exports = {
-  extends: [
-    'next/core-web-vitals',
-    'prettier',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+import nextPlugin from 'eslint-config-next/flat';
+
+export default [
+  ...nextPlugin,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
   },
-};
+  {
+    ignores: ['.next/', 'node_modules/'],
+  },
+];
